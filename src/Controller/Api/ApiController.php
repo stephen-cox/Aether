c825dda9f16b774e8ff8147743b0a0350eb59fba
Stephen Cox <contact@stephencox.net>
2021-04-04 08:37:49 +0100

    Added FOS REST bundle and basic api controller

---
+++ src/Controller/Api/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Api;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Root API controller class.
+ */
+class ApiController extends AbstractFOSRestController
+{
+    /**
+     * @Route("/", name="root")
+     */
+    public function index(Request $request)
+    {
+        $response = [
+            'status' => 'success',
+            'data' => [
+                'message' => 'Hello',
+                'api_version' => $request->attributes->get('version'),
+            ]
+        ];
+        $view = $this->view($response, 200);
+        return $this->handleView($view);
+    }
+}
