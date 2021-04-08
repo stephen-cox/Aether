8734aa78b4d72b03987457c13931625ccdbbf811
Stephen Cox <contact@stephencox.net>
2021-04-08 18:49:49 +0100

    Added tests

---
+++ tests/ApiTests.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiTests extends WebTestCase
+{
+    public function testRoot()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/v1/');
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $content = json_decode($response->getContent());
+        $this->assertEquals('success', $content->status);
+        $this->assertEquals('Hello', $content->data->message);
+        $this->assertEquals('v1', $content->data->api_version);
+    }
+}
