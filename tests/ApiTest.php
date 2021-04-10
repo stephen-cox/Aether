39f109f7714e9e178e81affa817440eed932442a
Stephen Cox <stephen@agile.coop>
2021-04-10 11:04:34 +0100

    Fixed PHPUnit test runs by requiring phpunit/phpunit:^8.5

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -4,7 +4,7 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ApiTests extends WebTestCase
+class ApiTest extends WebTestCase
 {
     public function testRoot()
     {
